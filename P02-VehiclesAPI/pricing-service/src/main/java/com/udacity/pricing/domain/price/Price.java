12fc2d49f75b4576ad40b447384c10c52110df0f
Plora Tran <ploratran@gmail.com>
2021-01-18 12:34:26 -0800

    success refactor and register Pricing service to microservice as Eureka client without H2 data

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,16 +1,28 @@
 package com.udacity.pricing.domain.price;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+
+@Entity
 public class Price {
+
+    // attributes:
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String currency;
     private BigDecimal price;
     private Long vehicleId;
 
+    // constructors:
     public Price() {
     }
 
@@ -20,6 +32,7 @@ public class Price {
         this.vehicleId = vehicleId;
     }
 
+    // getters and setters:
     public String getCurrency() {
         return currency;
     }
