56a6463e403f7f2959a2a66094c21c05d84327ba
Plora Tran <ploratran@gmail.com>
2021-01-19 10:59:46 -0800

    remove property id, use vehicleId for id of price

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,9 +1,6 @@
 package com.udacity.pricing.domain.price;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
@@ -16,11 +13,13 @@ public class Price {
     // attributes:
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Column(name="vehicleid") // define column attribute inside data.sql
+    private Long vehicleId;
 
     private String currency;
     private BigDecimal price;
-    private Long vehicleId;
+
+    // private Long vehicleId;
 
     // constructors:
     public Price() {
