c4f0df254aa63db29e0bad146907c44fe1b5aae9
Plora Tran <ploratran@gmail.com>
2021-01-19 20:47:56 -0800

    fix typo in unit test. Test still fail

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/tests/PricingControllerUnitTest.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/tests/PricingControllerUnitTest.java
@@ -7,8 +7,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.net.URI;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
@@ -30,9 +35,21 @@ public class PricingControllerUnitTest {
 
     @Test
     public void getPriceOfAVehicle() throws Exception {
-        this.mockMvc.perform(get("services/price?vehicleId=1"))
-                .andExpect(status().isOk());
+        this.mockMvc.perform(get("/services/price?vehicleId=1"))
+                .andExpect(status().isOk()); // set Expectations on HTTP responses status received from Controller class
 
-        verify(this.priceService, times(1));
+        // verifies the times (1 time) a mock method has been called
+        verify(this.priceService, times(1)).getPrice(1L);
     }
+
+    // TEST WITH JUNIT AND JUPITER WORKS
+//    @Test
+//    public void getPriceOfAVehicle() throws Exception {
+//        get(new URI("/services/price?vehicleId=1" ))
+//                .contentType(MediaType.APPLICATION_JSON_UTF8)
+//                .accept(MediaType.APPLICATION_JSON_UTF8);
+//        andExpect(status().isOk());
+//}
+//    private void andExpect(ResultMatcher ok) {
+//    }
 }
