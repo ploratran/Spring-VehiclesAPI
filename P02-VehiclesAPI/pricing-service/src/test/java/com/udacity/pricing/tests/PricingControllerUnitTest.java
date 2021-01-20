c111930f1ec04599ec432f167764749bd882281d
Plora Tran <ploratran@gmail.com>
2021-01-20 09:12:22 -0800

    change @MockBean to @Mock, Unit test for Pricing Service passed. Finish Pricing Service

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/tests/PricingControllerUnitTest.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/tests/PricingControllerUnitTest.java
@@ -4,16 +4,13 @@ import com.udacity.pricing.api.PricingController;
 import com.udacity.pricing.service.PricingService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-
-import java.net.URI;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
@@ -26,11 +23,7 @@ public class PricingControllerUnitTest {
     @Autowired
     private MockMvc mockMvc; // use MockMvc to quickly test MVC Controller without Http Server
 
-    /**
-     * @Mockbean: mocks Service layer Bean
-     * @Mockbean: creates Mockito mock of the services which are dependence of Controller
-     * */
-    @MockBean
+    @Mock
     PricingService priceService;
 
     @Test
@@ -41,15 +34,4 @@ public class PricingControllerUnitTest {
         // verifies the times (1 time) a mock method has been called
         verify(this.priceService, times(1)).getPrice(1L);
     }
-
-    // TEST WITH JUNIT AND JUPITER WORKS
-//    @Test
-//    public void getPriceOfAVehicle() throws Exception {
-//        get(new URI("/services/price?vehicleId=1" ))
-//                .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                .accept(MediaType.APPLICATION_JSON_UTF8);
-//        andExpect(status().isOk());
-//}
-//    private void andExpect(ResultMatcher ok) {
-//    }
 }
